87a51161f7fce562881eb3c4306d540baefce391
tgy1201 <154293542+tgy1201@users.noreply.github.com>
2024-01-18 06:13:58 +0900

    [Bronze I] Title: 단어 공부, Time: 128 ms, Memory: 41864 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,17 @@
+a = input()
+b = a.upper()
+count = 0
+ans = -1
+max = -1
+
+arr = [0]*len(b)
+
+for x in set(b):
+    arr.append(b.count(x))
+    if max < b.count(x):
+        max = b.count(x)
+        ans = x
+    elif b.count(x) == max:
+        ans = '?'
+else:
+    print(ans)
