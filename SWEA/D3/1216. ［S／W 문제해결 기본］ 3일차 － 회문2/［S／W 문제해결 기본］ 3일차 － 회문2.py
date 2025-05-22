e23d83575f9831b922ccdf9eb6fedc0220fdc04a
YuTaeGeun <dndsm82@kyonggi.ac.kr>
2025-05-23 08:22:01 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,225 ms, Memory: 89,984 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -21,35 +21,35 @@ for i in range(10):
         for k in range(100):
             if k % 2 == 0:
                 a = 1
-                while k + 2*a <= 100:
-                    if li[j][k:k+a] == li[j][k+a:k+2*a][::-1]:
-                        answer = max(answer, 2*a)
-                    if cp[j][k:k+a] == cp[j][k+a:k+2*a][::-1]:
-                        answer = max(answer, 2*a)
+                while k + 2 * a <= 100:
+                    if li[j][k:k + a] == li[j][k + (2 * a) - 1:k + a - 1:-1]:
+                        answer = max(answer, 2 * a)
+                    if cp[j][k:k + a] == cp[j][k + (2 * a) - 1:k + a - 1:-1]:
+                        answer = max(answer, 2 * a)
                     a += 1
 
                 b = 1
-                while k + 2*b + 1 <= 100:
-                    if li[j][k:k+b] == li[j][k+b+1:k+2*b+1][::-1]:
-                        answer = max(answer, 2*b+1)
-                    if cp[j][k:k+b] == cp[j][k+b+1:k+2*b+1][::-1]:
-                        answer = max(answer, 2*b+1)
+                while k + 2 * b + 1 <= 99:
+                    if li[j][k:k + b] == li[j][k + 2 * b:k + b:-1]:
+                        answer = max(answer, 2 * b + 1)
+                    if cp[j][k:k + b] == cp[j][k + 2 * b:k + b:-1]:
+                        answer = max(answer, 2 * b + 1)
                     b += 1
             else:
                 a = 1
-                while k + 2 * a <= 100:
-                    if li[j][k:k + a] == li[j][k+a:k+2*a][::-1]:
-                        answer = max(answer, 2*a)
-                    if cp[j][k:k + a] == cp[j][k+a:k+2*a][::-1]:
-                        answer = max(answer, 2*a)
+                while k + 2 * a <= 99:
+                    if li[j][k:k + a] == li[j][k + (2 * a) - 1:k + a - 1:-1]:
+                        answer = max(answer, 2 * a)
+                    if cp[j][k:k + a] == cp[j][k + (2 * a) - 1:k + a - 1:-1]:
+                        answer = max(answer, 2 * a)
                     a += 1
 
                 b = 1
                 while k + 2 * b + 1 <= 100:
-                    if li[j][k:k + b] == li[j][k+b+1:k+2*b+1][::-1]:
-                        answer = max(answer, 2*b+1)
-                    if cp[j][k:k + b] == cp[j][k+b+1:k+2*b+1][::-1]:
-                        answer = max(answer, 2*b+1)
+                    if li[j][k:k + b] == li[j][k + 2 * b:k + b:-1]:
+                        answer = max(answer, 2 * b + 1)
+                    if cp[j][k:k + b] == cp[j][k + 2 * b:k + b:-1]:
+                        answer = max(answer, 2 * b + 1)
                     b += 1
 
     print(f"#{N} {answer}")
