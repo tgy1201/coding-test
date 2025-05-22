bcbfa399a3d08783cef2b7ee157b36641d0c96a7
YuTaeGeun <dndsm82@kyonggi.ac.kr>
2025-05-23 08:19:13 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,586 ms, Memory: 92,160 KB -BaekjoonHub

---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -0,0 +1,56 @@
+for i in range(10):
+    N = int(input())
+
+    li = []
+
+    for j in range(100):
+        e = input()
+        ll = []
+        for k in e:
+            ll.append(k)
+        li.append(ll)
+
+    cp = [["0"]*100 for _ in range(100)]
+    for j in range(100):
+        for k in range(100):
+            cp[j][k] = li[k][j]
+
+    answer = -1
+
+    for j in range(100):
+        for k in range(100):
+            if k % 2 == 0:
+                a = 1
+                while k + 2*a <= 100:
+                    if li[j][k:k+a] == li[j][k+a:k+2*a][::-1]:
+                        answer = max(answer, 2*a)
+                    if cp[j][k:k+a] == cp[j][k+a:k+2*a][::-1]:
+                        answer = max(answer, 2*a)
+                    a += 1
+
+                b = 1
+                while k + 2*b + 1 <= 100:
+                    if li[j][k:k+b] == li[j][k+b+1:k+2*b+1][::-1]:
+                        answer = max(answer, 2*b+1)
+                    if cp[j][k:k+b] == cp[j][k+b+1:k+2*b+1][::-1]:
+                        answer = max(answer, 2*b+1)
+                    b += 1
+            else:
+                a = 1
+                while k + 2 * a <= 100:
+                    if li[j][k:k + a] == li[j][k+a:k+2*a][::-1]:
+                        answer = max(answer, 2*a)
+                    if cp[j][k:k + a] == cp[j][k+a:k+2*a][::-1]:
+                        answer = max(answer, 2*a)
+                    a += 1
+
+                b = 1
+                while k + 2 * b + 1 <= 100:
+                    if li[j][k:k + b] == li[j][k+b+1:k+2*b+1][::-1]:
+                        answer = max(answer, 2*b+1)
+                    if cp[j][k:k + b] == cp[j][k+b+1:k+2*b+1][::-1]:
+                        answer = max(answer, 2*b+1)
+                    b += 1
+
+    print(f"#{N} {answer}")
+#shift+F10
